dc35d4109817474a3dbef06a34c38d311122d572
Saunak Ghosh <saunakghosh710@gmail.com>
2024-11-30 20:26:35 +0530

    dfolio v1

---
+++ app/about-section/songDetails.ts
@@ -0,0 +1,69 @@
+export type SongProps = {
+  title: string;
+  artist: string;
+  image: string;
+  link: string;
+};
+
+export const songs = [
+  {
+    title: "Wishes",
+    artist: "Hasan Raheem",
+    image: require(".//../../public/songs/wishes.png"),
+    link: "https://open.spotify.com/track/5rpCUsEfBLIumvrxrahnKF?si=55a7d5af21174586",
+  },
+  {
+    title: "Samjho Na",
+    artist: "Aditya Rikhari",
+    image: require(".//../../public/songs/samjhona.png"),
+    link: "https://open.spotify.com/track/1d5lpW3gUyq537iuyrb9Lf?si=8299104e5b70499f",
+  },
+  {
+    title: "Where are You Now",
+    artist: "Skrillex, Diplo, Justin Bieber",
+    image: require(".//../../public/songs/whereu.png"),
+    link: "https://open.spotify.com/track/66hayvUbTotekKU3H4ta1f?si=6220af5c77414354",
+  },
+  {
+    title: "Iraaday",
+    artist: "Abdul Hanan,Rovalio",
+    image: require(".//../../public/songs/iraaday.png"),
+    link: "https://open.spotify.com/track/6qrifdo7QINdPQr80IelGi?si=9e59132c1c6145f1",
+  },
+  {
+    title: "Legends Never Die",
+    artist: "Against The Current",
+    image: require(".//../../public/songs/legedns.png"),
+    link: "https://open.spotify.com/track/1FpVJ7HpZInE2GvhVE2TwT?si=fde7b28561bd44cc",
+  },
+  {
+    title: "Insane",
+    artist: "AP Dhillon",
+    image: require(".//../../public/songs/insane.png"),
+    link: "https://open.spotify.com/track/3UhmuWcFaLS3AO3kI9ynQ2?si=5192ad230924485f",
+  },
+  {
+    title: "Starboy",
+    artist: "The Weeknd",
+    image: require(".//../../public/songs/starboy.png"),
+    link: "https://open.spotify.com/track/7MXVkk9YMctZqd1Srtv4MB?si=9241b137b1f94386",
+  },
+  {
+    title: "Dildara",
+    artist: "Shafqat Amanat Ali",
+    image: require(".//../../public/songs/didara.png"),
+    link: "https://open.spotify.com/track/6ZoCBDOB308GqUdt2AJrV2?si=9c2447e50e934067",
+  },
+  {
+    title: "Tumhare Rahenge Hum",
+    artist: "Sachin Jigar",
+    image: require(".//../../public/songs/stree2.png"),
+    link: "https://open.spotify.com/track/5Ggb9pKiKcr5vNV9v1e1vs?si=1b34bd51a79849bd",
+  },
+  {
+    title: "New Divide",
+    artist: "Linkin Park",
+    image: require(".//../../public/songs/divide.png"),
+    link: "https://open.spotify.com/track/6Wx88Mv6b9ofjKMKkdwOJd?si=6cab7a73bd214947",
+  },
+];
---
+++ app/blog-section/blogDetails.ts
@@ -0,0 +1,71 @@
+export type blogProps = {
+  title: string;
+  image: string;
+  date: string;
+  url: string;
+  available: boolean;
+  index: number;
+};
+
+export const blogDetails = [
+  // {
+  //   title: "Building a Text Field Component with Material UI, Formik, and Yup",
+  //   image:
+  //     "https://cdn.hashnode.com/res/hashnode/image/upload/v1628442954056/OHNFYAzz4.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+  //   date: "12/31/2022",
+  //   url: "",
+  //   available: false,
+  // },
+  // {
+  //   title:
+  //     "Creating a Select Field Component with Material UI, Formik, and Yup",
+  //   image:
+  //     "https://cdn.hashnode.com/res/hashnode/image/upload/v1624616590157/N4Hvd8NfZ.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+  //   date: "12/31/2022",
+  //   url: "",
+  //   available: false,
+  // },
+  {
+    title: "Leveling Up Your Next.js App with Framer Motion: A Dev's Journey",
+    image:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1725811203184/e492f112-150b-4e75-adcd-2fe1cd1a4dcf.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    date: "SEPT 2024",
+    url: "https://rivuu.hashnode.dev/leveling-up-your-nextjs-app-with-framer-motion-a-devs-journey",
+    available: true,
+  },
+  {
+    title:
+      "The Future of AI in Software Development: Opportunities and Challenges",
+    image:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1725811817888/58315212-4f17-4439-880a-09cf57955cc9.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    date: "SEPT 2024",
+    url: "https://rivuu.hashnode.dev/the-future-of-ai-in-software-development-opportunities-and-challenges",
+    available: true,
+  },
+  {
+    title:
+      "Micro Frontends: A deep dive into how micro frontends are enabling scalable and maintainable web applications",
+    image:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1725812293416/76d2b426-6bd0-4b7f-8161-4187231b2d99.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+    date: "SEPT 2024",
+    url: "https://rivuu.hashnode.dev/micro-frontends-a-deep-dive-into-how-micro-frontends-are-enabling-scalable-and-maintainable-web-applications-allowing-teams-to-work-independently",
+    available: true,
+  },
+  // {
+  //   title:
+  //     "2023: A Year in Review – Navigating Challenges and Embracing Growth",
+  //   image:
+  //     "https://cdn.hashnode.com/res/hashnode/image/upload/v1704146853987/5e5ffc0f-9d20-4cc8-ad94-00ec737ba0f5.jpeg",
+  //   date: "JAN 2024",
+  //   url: "https://blog.victorwilliams.me/2023-a-year-in-review",
+  //   available: true,
+  // },
+  // {
+  //   title: "Reflecting on My 18th Year: A Personal Review of 2022",
+  //   image:
+  //     "https://cdn.hashnode.com/res/hashnode/image/upload/v1672494063925/1ee2925f-e529-4a8b-9dd7-6eea996a34eb.png",
+  //   date: "DEC 2022",
+  //   url: "https://blog.victorwilliams.me/reflecting-on-my-18th-year-a-personal-review-of-2022",
+  //   available: true,
+  // },
+];
